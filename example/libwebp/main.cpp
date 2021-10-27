53642dacc914ba8c8f85eb628473249d013b2d8b
gongluck <1039994845@qq.com>
2021-10-27 16:48:41 +0800

    优化libwebp例子

--- example/libwebp/main.cpp
+++ example/libwebp/main.cpp
@@ -2,7 +2,7 @@
  * @Author: gongluck
  * @Date: 2021-10-25 16:43:06
  * @Last Modified by: gongluck
- * @Last Modified time: 2021-10-25 17:55:26
+ * @Last Modified time: 2021-10-27 16:13:26
  */
 
 #include <iostream>
@@ -29,107 +29,124 @@ int main(int argc, char *argv[])
 	const int width = atoi(argv[2]);
 	const int height = atoi(argv[3]);
 	std::ofstream outfile(argv[4], std::ios::binary);
-	int outlen = 0;
+	int webplen = 0;
 
 	auto framesize = width * height * 3 / 2;
 	yuvin.seekg(framesize * 20);
 	uint8_t *indata = static_cast<uint8_t *>(malloc(framesize));
 	yuvin.read(reinterpret_cast<char *>(indata), framesize);
 
+	int eret = 0;
 	// encode
-	WebPPicture epic;
 	WebPConfig econfig;
-	WebPMemoryWriter ewrt;
-	if (!WebPConfigPreset(&econfig, WEBP_PRESET_DEFAULT, 10.0f) ||
-		!WebPPictureInit(&epic))
+	eret = WebPConfigPreset(&econfig, WEBP_PRESET_DEFAULT, 10.0f);
+	if (eret != 1)
 	{
-		std::cerr << "WebPConfigPreset or WebPPictureInit failed." << std::endl;
+		std::cerr << "WebPConfigPreset failed, error:" << eret << std::endl;
 		return -1;
 	}
-
-	econfig.lossless = 0;		  //是否无损
-	econfig.quality = 50;		  //无损时可调节编码复杂度
+	econfig.sns_strength = 0;
+	econfig.filter_sharpness = 7;
+	econfig.alpha_quality = 100;
 	WebPValidateConfig(&econfig); // not mandatory, but useful
+
+	WebPPicture epic;
+	eret = WebPPictureInit(&epic);
+	if (eret != 1)
+	{
+		std::cerr << "WebPPictureInit failed, error:" << eret << std::endl;
+		return -1;
+	}
 	epic.use_argb = 0;
 	epic.width = width;
 	epic.height = height;
-	epic.writer = WebPMemoryWrite;
-	epic.custom_ptr = &ewrt;
-	WebPMemoryWriterInit(&ewrt);
-	if (!WebPPictureAlloc(&epic))
+	eret = WebPPictureAlloc(&epic);
+	if (eret != 1)
 	{
-		std::cerr << "WebPPictureAlloc failed." << std::endl;
+		std::cerr << "WebPPictureAlloc failed, error:" << eret << std::endl;
 		return -1;
 	}
 
-	// 获取y、u、v三个分量各自数据的指针地址
-	unsigned char *ybase, *ubase, *vbase;
-	ybase = indata;
-	ubase = indata + width * height;
-	vbase = indata + width * height * 5 / 4;
-	int ystride = width;
-	int ustride = width / 2;
-	int vstride = width / 2;
+	WebPMemoryWriter writer;
+	WebPMemoryWriterInit(&writer);
+	epic.writer = WebPMemoryWrite;
+	epic.custom_ptr = &writer;
 
-	int dest_ystride = epic.y_stride;
-	int dest_ustride = epic.uv_stride;
-	int dest_vstride = epic.uv_stride;
-	int widthuv = width / 2;
-
+	// 拷贝数据
+	unsigned char *ybase = indata;
 	unsigned char *dest_y = epic.y;
 	for (int i = 0; i < height; i++)
 	{
 		memcpy(dest_y, ybase, width);
-		ybase += ystride;
-		dest_y += dest_ystride;
+		ybase += width;
+		dest_y += epic.y_stride;
 	}
 
+	unsigned char *ubase = indata + width * height;
+	unsigned char *vbase = indata + width * height * 5 / 4;
 	unsigned char *dest_u = epic.u;
 	unsigned char *dest_v = epic.v;
 	for (int i = 0; i < height / 2; i++)
 	{
-		memcpy(dest_u, ubase, widthuv);
-		memcpy(dest_v, vbase, widthuv);
-		ubase += ustride;
-		dest_u += dest_ustride;
-		vbase += vstride;
-		dest_v += dest_vstride;
+		memcpy(dest_u, ubase, width / 2);
+		memcpy(dest_v, vbase, width / 2);
+		ubase += width / 2;
+		dest_u += epic.uv_stride;
+		vbase += width / 2;
+		dest_v += epic.uv_stride;
 	}
 
-	if (!WebPEncode(&econfig, &epic))
+	eret = WebPEncode(&econfig, &epic);
+	if (eret != 1)
 	{
-		std::cerr << "WebPEncode failed." << std::endl;
+		std::cerr << "WebPEncode failed, error:" << eret << std::endl;
 		return -1;
 	}
-	outfile.write(reinterpret_cast<char *>(ewrt.mem), ewrt.size);
-	outlen = ewrt.size;
-	WebPMemoryWriterClear(&ewrt);
+
+	outfile.write(reinterpret_cast<char *>(writer.mem), writer.size);
+	outfile.close();
+	webplen = writer.size;
+	WebPMemoryWriterClear(&writer);
 	WebPPictureFree(&epic);
 
+	yuvin.close();
 	yuvin.open(argv[4], std::ios::binary);
-	yuvin.read(reinterpret_cast<char *>(indata), outlen);
+	yuvin.read(reinterpret_cast<char *>(indata), webplen);
+	int dret = 0;
 	// decode
 	WebPDecoderConfig dconfig;
-	WebPDecBuffer *output_buffer = &dconfig.output;
-	WebPBitstreamFeatures *bitstream = &dconfig.input;
-	if (!WebPInitDecoderConfig(&dconfig))
+	dret = WebPInitDecoderConfig(&dconfig);
+	if (dret != 1)
 	{
-		std::cerr << "WebPInitDecoderConfig failed." << std::endl;
+		std::cerr << "WebPInitDecoderConfig failed, error:" << dret << std::endl;
 		return -1;
 	}
 
-	output_buffer->colorspace = MODE_RGBA;
-	dconfig.output.colorspace = MODE_RGBA;
-	dconfig.options.use_threads = 1;
-	if (WebPDecode(indata, outlen, &dconfig) != VP8_STATUS_OK)
+	/*dret = WebPGetFeatures(indata, webplen, &dconfig.input);
+	if (dret != VP8_STATUS_OK)
 	{
-		std::cerr << "WebPDecode failed." << std::endl;
+		std::cerr << "WebPGetFeatures failed, error:" << dret << std::endl;
+		return -1;
+	}*/
+
+	dconfig.output.colorspace = MODE_RGB;
+	dconfig.options.use_threads = 1;
+	// dconfig.output.u.RGBA.rgba = (uint8_t*)malloc(width*height*3);
+	// dconfig.output.u.RGBA.stride = width*3;
+	// dconfig.output.u.RGBA.size = width * height * 3;
+	// dconfig.output.is_external_memory = 1;
+
+	dret = WebPDecode(indata, webplen, &dconfig);
+	if (dret != VP8_STATUS_OK)
+	{
+		std::cerr << "WebPDecode failed, error:" << dret << std::endl;
 		return -1;
 	}
 
 	outfile.open("./out.rgba", std::ios::binary);
-	outfile.write(reinterpret_cast<char *>(output_buffer->u.RGBA.rgba), output_buffer->u.RGBA.size);
-	WebPFreeDecBuffer(output_buffer);
+	outfile.write(reinterpret_cast<char *>(dconfig.output.u.RGBA.rgba), dconfig.output.u.RGBA.size);
+	// free(dconfig.output.u.RGBA.rgba);
+	WebPFreeDecBuffer(&dconfig.output);
 
 	free(indata);
 	outfile.close();
