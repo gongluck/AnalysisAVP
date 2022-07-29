4cefe5f7bc491b5a7715296274c7924cf9083fcd
gongluck <gongluck93@gmail.com>
2022-07-29 21:47:29 +0800

    update

--- analysis/h264/h264.cpp
+++ analysis/h264/h264.cpp
@@ -9,94 +9,94 @@
 
 const char *nal_parse_idc(uint8_t idc)
 {
-	switch (idc)
-	{
-	case NAL_REF_IDC_PRIORITY_HIGHEST:
-		return "HIGHEST";
-	case NAL_REF_IDC_PRIORITY_HIGH:
-		return "HEIGHT";
-	case NAL_REF_IDC_PRIORITY_LOW:
-		return "LOW";
-	case NAL_REF_IDC_PRIORITY_DISPOSABLE:
-		return "DISPOSABLE";
-	default:
-		return "UNKNOWN";
-	}
+  switch (idc)
+  {
+  case NAL_REF_IDC_PRIORITY_HIGHEST:
+    return "HIGHEST";
+  case NAL_REF_IDC_PRIORITY_HIGH:
+    return "HEIGHT";
+  case NAL_REF_IDC_PRIORITY_LOW:
+    return "LOW";
+  case NAL_REF_IDC_PRIORITY_DISPOSABLE:
+    return "DISPOSABLE";
+  default:
+    return "UNKNOWN";
+  }
 }
 
 const char *nal_parse_type(uint8_t type)
 {
-	switch (type)
-	{
-	case NAL_UNIT_TYPE_UNSPECIFIED:
-		return "Unspecified";
-	case NAL_UNIT_TYPE_CODED_SLICE_NON_IDR:
-		return "Coded slice of a non-IDR picture";
-	case NAL_UNIT_TYPE_CODED_SLICE_DATA_PARTITION_A:
-		return "Coded slice data partition A";
-	case NAL_UNIT_TYPE_CODED_SLICE_DATA_PARTITION_B:
-		return "Coded slice data partition B";
-	case NAL_UNIT_TYPE_CODED_SLICE_DATA_PARTITION_C:
-		return "Coded slice data partition C";
-	case NAL_UNIT_TYPE_CODED_SLICE_IDR:
-		return "Coded slice of an IDR picture";
-	case NAL_UNIT_TYPE_SEI:
-		return "Supplemental enhancement information (SEI)";
-	case NAL_UNIT_TYPE_SPS:
-		return "Sequence parameter set";
-	case NAL_UNIT_TYPE_PPS:
-		return "Picture parameter set";
-	case NAL_UNIT_TYPE_AUD:
-		return "Access unit delimiter";
-	case NAL_UNIT_TYPE_END_OF_SEQUENCE:
-		return "End of sequence";
-	case NAL_UNIT_TYPE_END_OF_STREAM:
-		return "End of stream";
-	case NAL_UNIT_TYPE_FILLER:
-		return "Filler data";
-	case NAL_UNIT_TYPE_SPS_EXT:
-		return "Sequence parameter set extension";
-	case NAL_UNIT_TYPE_CODED_SLICE_AUX:
-		return "Coded slice of an auxiliary coded picture without partitioning";
-	default:
-		return "UNKNOWN";
-	}
+  switch (type)
+  {
+  case NAL_UNIT_TYPE_UNSPECIFIED:
+    return "Unspecified";
+  case NAL_UNIT_TYPE_CODED_SLICE_NON_IDR:
+    return "Coded slice of a non-IDR picture";
+  case NAL_UNIT_TYPE_CODED_SLICE_DATA_PARTITION_A:
+    return "Coded slice data partition A";
+  case NAL_UNIT_TYPE_CODED_SLICE_DATA_PARTITION_B:
+    return "Coded slice data partition B";
+  case NAL_UNIT_TYPE_CODED_SLICE_DATA_PARTITION_C:
+    return "Coded slice data partition C";
+  case NAL_UNIT_TYPE_CODED_SLICE_IDR:
+    return "Coded slice of an IDR picture";
+  case NAL_UNIT_TYPE_SEI:
+    return "Supplemental enhancement information (SEI)";
+  case NAL_UNIT_TYPE_SPS:
+    return "Sequence parameter set";
+  case NAL_UNIT_TYPE_PPS:
+    return "Picture parameter set";
+  case NAL_UNIT_TYPE_AUD:
+    return "Access unit delimiter";
+  case NAL_UNIT_TYPE_END_OF_SEQUENCE:
+    return "End of sequence";
+  case NAL_UNIT_TYPE_END_OF_STREAM:
+    return "End of stream";
+  case NAL_UNIT_TYPE_FILLER:
+    return "Filler data";
+  case NAL_UNIT_TYPE_SPS_EXT:
+    return "Sequence parameter set extension";
+  case NAL_UNIT_TYPE_CODED_SLICE_AUX:
+    return "Coded slice of an auxiliary coded picture without partitioning";
+  default:
+    return "UNKNOWN";
+  }
 }
 
 std::ostream &operator<<(std::ostream &os, const NALHEADER &nalheader)
 {
-	os << "nal_ref_idc : " << nal_parse_idc(nalheader.nal_ref_idc)
-		 << "\nnal_unit_type : " << nal_parse_type(nalheader.nal_unit_type);
-	return os;
+  os << "nal_ref_idc : " << nal_parse_idc(nalheader.nal_ref_idc)
+     << "\nnal_unit_type : " << nal_parse_type(nalheader.nal_unit_type);
+  return os;
 }
 
 //[start, end)
 int32_t findnalu(uint8_t *data, uint32_t start, uint32_t end, int8_t *nalstep)
 {
-	if (start >= end)
-	{
-		return -1;
-	}
+  if (start >= end)
+  {
+    return -1;
+  }
 
-	uint32_t i = start;
-	//*nalstep = 0;//记录0x00的个数
-	for (; i < end; ++i)
-	{
-		if (data[i] == 0)
-		{
-			++*nalstep;
-		}
-		else if (data[i] == 1 && *nalstep >= 2)
-		{
-			*nalstep = *nalstep > 2 ? 4 : 3;
-			break;
-		}
-		else
-		{
-			*nalstep = 0;
-		}
-	}
+  uint32_t i = start;
+  //*nalstep = 0;//记录0x00的个数
+  for (; i < end; ++i)
+  {
+    if (data[i] == 0)
+    {
+      ++*nalstep;
+    }
+    else if (data[i] == 1 && *nalstep >= 2)
+    {
+      *nalstep = *nalstep > 2 ? 4 : 3;
+      break;
+    }
+    else
+    {
+      *nalstep = 0;
+    }
+  }
 
-	// i有效时返回0x01的索引
-	return i >= end ? -1 : i;
+  // i有效时返回0x01的索引
+  return i >= end ? -1 : i;
 }
--- analysis/h264/main.cpp
+++ analysis/h264/main.cpp
@@ -16,192 +16,192 @@
 
 int main(int argc, char *argv[])
 {
-	std::cout << "h264 analysis" << std::endl;
+  std::cout << "h264 analysis" << std::endl;
 
-	std::cout << "Usage : "
-						<< "thisfile h264file." << std::endl;
+  std::cout << "Usage : "
+            << "thisfile h264file." << std::endl;
 
-	if (argc < 2)
-	{
-		std::cerr << "please see the usage message." << std::endl;
-		return -1;
-	}
-	std::ifstream in(argv[1], std::ios::binary);
-	if (in.fail())
-	{
-		std::cerr << "can not open file " << argv[1] << std::endl;
-		return -1;
-	}
+  if (argc < 2)
+  {
+    std::cerr << "please see the usage message." << std::endl;
+    return -1;
+  }
+  std::ifstream in(argv[1], std::ios::binary);
+  if (in.fail())
+  {
+    std::cerr << "can not open file " << argv[1] << std::endl;
+    return -1;
+  }
 
-	unsigned char c = 0;
-	int32_t datalen = 0;
-	int step = 0; //记录0x00的个数
-	while (in.read(reinterpret_cast<char *>(&c), 1))
-	{
-		if (c == 0)
-		{
-			++step;
-		}
-		else if (c == 1 && step >= 2)
-		{
-			std::streamoff naluflagsize = step > 2 ? 4 : 3;
-			datalen += (step > 3 ? step - 3 : 0);
-			if (datalen != 0)
-			{
-				std::cout << std::string(50, '*').c_str() << std::endl;
-				std::cout << "nalu size : " << datalen << std::endl;
+  unsigned char c = 0;
+  int32_t datalen = 0;
+  int step = 0; //记录0x00的个数
+  while (in.read(reinterpret_cast<char *>(&c), 1))
+  {
+    if (c == 0)
+    {
+      ++step;
+    }
+    else if (c == 1 && step >= 2)
+    {
+      std::streamoff naluflagsize = step > 2 ? 4 : 3;
+      datalen += (step > 3 ? step - 3 : 0);
+      if (datalen != 0)
+      {
+        std::cout << std::string(50, '*').c_str() << std::endl;
+        std::cout << "nalu size : " << datalen << std::endl;
 #if PRINTDATA
-				in.seekg(-naluflagsize - datalen, std::ios::cur);
-				unsigned char *naludata = static_cast<unsigned char *>(malloc(datalen));
-				if (!in.read(reinterpret_cast<char *>(naludata), datalen))
-					break;
-				NALHEADER *pnalheader = reinterpret_cast<NALHEADER *>(naludata);
-				std::cout << *pnalheader << std::endl;
-				std::ios::fmtflags f(std::cout.flags());
-				for (int i = 0; i < datalen; ++i)
-				{
-					std::cout << std::setw(2) << std::setfill('0') << std::hex << static_cast<unsigned int>(naludata[i]) << " ";
-				}
-				std::cout << std::endl;
-				std::cout.flags(f);
-				in.seekg(naluflagsize, std::ios::cur);
-				free(naludata);
+        in.seekg(-naluflagsize - datalen, std::ios::cur);
+        unsigned char *naludata = static_cast<unsigned char *>(malloc(datalen));
+        if (!in.read(reinterpret_cast<char *>(naludata), datalen))
+          break;
+        NALHEADER *pnalheader = reinterpret_cast<NALHEADER *>(naludata);
+        std::cout << *pnalheader << std::endl;
+        std::ios::fmtflags f(std::cout.flags());
+        for (int i = 0; i < datalen; ++i)
+        {
+          std::cout << std::setw(2) << std::setfill('0') << std::hex << static_cast<unsigned int>(naludata[i]) << " ";
+        }
+        std::cout << std::endl;
+        std::cout.flags(f);
+        in.seekg(naluflagsize, std::ios::cur);
+        free(naludata);
 #endif
-			}
-			datalen = 0;
-			step = 0;
-		}
-		else
-		{
-			datalen += step + 1;
-			step = 0;
-		}
-	}
-	std::cout << std::string(50, '*').c_str() << std::endl;
-	std::cout << "nalu size : " << datalen << std::endl;
+      }
+      datalen = 0;
+      step = 0;
+    }
+    else
+    {
+      datalen += step + 1;
+      step = 0;
+    }
+  }
+  std::cout << std::string(50, '*').c_str() << std::endl;
+  std::cout << "nalu size : " << datalen << std::endl;
 #if PRINTDATA
-	in.close();
-	in.open(argv[1], std::ios::binary);
-	in.seekg(-datalen, std::ios::end);
-	unsigned char *naludata = static_cast<unsigned char *>(malloc(datalen));
-	in.read(reinterpret_cast<char *>(naludata), datalen);
-	NALHEADER *pnalheader = reinterpret_cast<NALHEADER *>(naludata);
-	std::cout << *pnalheader << std::endl;
-	std::ios::fmtflags f(std::cout.flags());
-	for (int i = 0; i < datalen; ++i)
-	{
-		std::cout << std::setw(2) << std::setfill('0') << std::hex << static_cast<unsigned int>(naludata[i]) << " ";
-	}
-	std::cout << std::endl;
-	std::cout.flags(f);
-	free(naludata);
+  in.close();
+  in.open(argv[1], std::ios::binary);
+  in.seekg(-datalen, std::ios::end);
+  unsigned char *naludata = static_cast<unsigned char *>(malloc(datalen));
+  in.read(reinterpret_cast<char *>(naludata), datalen);
+  NALHEADER *pnalheader = reinterpret_cast<NALHEADER *>(naludata);
+  std::cout << *pnalheader << std::endl;
+  std::ios::fmtflags f(std::cout.flags());
+  for (int i = 0; i < datalen; ++i)
+  {
+    std::cout << std::setw(2) << std::setfill('0') << std::hex << static_cast<unsigned int>(naludata[i]) << " ";
+  }
+  std::cout << std::endl;
+  std::cout.flags(f);
+  free(naludata);
 #endif
-	in.close();
+  in.close();
 
-	uint8_t data[] = {
-			0x00,
-			0x00,
-			0x00,
-			0x01,
-			0x12,
-			0x45,
-			0x55,
-			0x55,
-			0x00,
-			0x00,
-			0x01,
-			0x01,
-			0x18,
-			0x45,
-			0x55,
-			0x55,
-			0x00,
-			0x00,
-			0x00,
-			0x01,
-			0x00,
-			0x00,
-			0x01,
-			0x01,
-	};
-	int8_t nalstep = 0;
-	int ret = 0;
-	int tmp = ret;
-	while (ret >= 0)
-	{
-		tmp = ret;
-		ret = findnalu(data, tmp, sizeof(data), &nalstep);
-		if (ret >= 0 && ret < sizeof(data))
-		{
-			std::ios::fmtflags f(std::cout.flags());
-			for (int i = tmp; i > 0 && i < ret - nalstep; ++i)
-			{
-				std::cout << std::setw(2) << std::setfill('0') << std::hex << static_cast<unsigned int>(data[i]) << " ";
-			}
-			std::cout << std::endl;
-			std::cout.flags(f);
-			nalstep = 0;
-		}
-	}
-	std::ios::fmtflags f(std::cout.flags());
-	for (int i = tmp + 1; i > 0 && i < sizeof(data); ++i)
-	{
-		std::cout << std::setw(2) << std::setfill('0') << std::hex << static_cast<unsigned int>(data[i]) << " ";
-	}
-	std::cout << std::endl;
-	std::cout.flags(f);
+  uint8_t data[] = {
+      0x00,
+      0x00,
+      0x00,
+      0x01,
+      0x12,
+      0x45,
+      0x55,
+      0x55,
+      0x00,
+      0x00,
+      0x01,
+      0x01,
+      0x18,
+      0x45,
+      0x55,
+      0x55,
+      0x00,
+      0x00,
+      0x00,
+      0x01,
+      0x00,
+      0x00,
+      0x01,
+      0x01,
+  };
+  int8_t nalstep = 0;
+  int ret = 0;
+  int tmp = ret;
+  while (ret >= 0)
+  {
+    tmp = ret;
+    ret = findnalu(data, tmp, sizeof(data), &nalstep);
+    if (ret >= 0 && ret < sizeof(data))
+    {
+      std::ios::fmtflags f(std::cout.flags());
+      for (int i = tmp; i > 0 && i < ret - nalstep; ++i)
+      {
+        std::cout << std::setw(2) << std::setfill('0') << std::hex << static_cast<unsigned int>(data[i]) << " ";
+      }
+      std::cout << std::endl;
+      std::cout.flags(f);
+      nalstep = 0;
+    }
+  }
+  std::ios::fmtflags f(std::cout.flags());
+  for (int i = tmp + 1; i > 0 && i < sizeof(data); ++i)
+  {
+    std::cout << std::setw(2) << std::setfill('0') << std::hex << static_cast<unsigned int>(data[i]) << " ";
+  }
+  std::cout << std::endl;
+  std::cout.flags(f);
 
-	in.open(argv[1], std::ios::binary);
-	if (in.fail())
-	{
-		std::cerr << "can not open file " << argv[1] << std::endl;
-		return -1;
-	}
+  in.open(argv[1], std::ios::binary);
+  if (in.fail())
+  {
+    std::cerr << "can not open file " << argv[1] << std::endl;
+    return -1;
+  }
 
-	char buf[1024] = {0};
-	ret = tmp = 0;
-	datalen = 0;
-	nalstep = 0;
-	int readlen = 0;
-	while (in.read(reinterpret_cast<char *>(buf), sizeof(buf)))
-	{
-		readlen = in.gcount();
-		while (ret >= 0)
-		{
-			tmp = ret;
-			ret = findnalu(reinterpret_cast<uint8_t *>(buf), tmp, readlen, &nalstep);
-			if (ret >= 0 && ret < readlen)
-			{
-				datalen += ret - tmp - nalstep;
-				std::cout << std::string(50, '*').c_str() << std::endl;
-				std::cout << "nalu size : " << datalen << std::endl;
-				datalen = 0;
-				nalstep = 0;
-				tmp = 0;
-			}
-		}
-		ret = 0;
-		datalen += readlen - tmp;
-	}
-	readlen = in.gcount();
-	while (ret >= 0)
-	{
-		tmp = ret;
-		ret = findnalu(reinterpret_cast<uint8_t *>(buf), tmp, readlen, &nalstep);
-		if (ret >= 0 && ret < readlen)
-		{
-			datalen += ret - tmp - nalstep;
-			std::cout << std::string(50, '*').c_str() << std::endl;
-			std::cout << "nalu size : " << datalen << std::endl;
-			datalen = 0;
-			nalstep = 0;
-			tmp = 0;
-		}
-	}
-	datalen += readlen - tmp - 1;
-	std::cout << std::string(50, '*').c_str() << std::endl;
-	std::cout << "nalu size : " << datalen << std::endl;
+  char buf[1024] = {0};
+  ret = tmp = 0;
+  datalen = 0;
+  nalstep = 0;
+  int readlen = 0;
+  while (in.read(reinterpret_cast<char *>(buf), sizeof(buf)))
+  {
+    readlen = in.gcount();
+    while (ret >= 0)
+    {
+      tmp = ret;
+      ret = findnalu(reinterpret_cast<uint8_t *>(buf), tmp, readlen, &nalstep);
+      if (ret >= 0 && ret < readlen)
+      {
+        datalen += ret - tmp - nalstep;
+        std::cout << std::string(50, '*').c_str() << std::endl;
+        std::cout << "nalu size : " << datalen << std::endl;
+        datalen = 0;
+        nalstep = 0;
+        tmp = 0;
+      }
+    }
+    ret = 0;
+    datalen += readlen - tmp;
+  }
+  readlen = in.gcount();
+  while (ret >= 0)
+  {
+    tmp = ret;
+    ret = findnalu(reinterpret_cast<uint8_t *>(buf), tmp, readlen, &nalstep);
+    if (ret >= 0 && ret < readlen)
+    {
+      datalen += ret - tmp - nalstep;
+      std::cout << std::string(50, '*').c_str() << std::endl;
+      std::cout << "nalu size : " << datalen << std::endl;
+      datalen = 0;
+      nalstep = 0;
+      tmp = 0;
+    }
+  }
+  datalen += readlen - tmp - 1;
+  std::cout << std::string(50, '*').c_str() << std::endl;
+  std::cout << "nalu size : " << datalen << std::endl;
 
-	in.close();
-	return 0;
+  in.close();
+  return 0;
 }
