b557eef541b247f95d2e249b52ca5b78350aee00
gongluck93@gmail.com <gongluck93@gmail.com>
2023-02-17 15:45:09 +0800

    libopus

--- example/build_android.bat
+++ example/build_android.bat
@@ -5,47 +5,22 @@ set buildprefix=./build/android
 set buildparams=-DCMAKE_TOOLCHAIN_FILE="%ANDROID_NDK_HOME%/build/cmake/android.toolchain.cmake" -DCMAKE_MAKE_PROGRAM=%ANDROID_NDK_HOME%/prebuilt/windows-x86_64/bin/make.exe -DANDROID_NDK=%ANDROID_NDK_HOME% -DANDROID_PLATFORM=android-22
 
 rem armeabi-v7a
-set abi=armeabi-v7a
-set type=debug
-set buildpath=%buildprefix%/%abi%_%type%
-cmake -G "Unix Makefiles" -S . -B %buildpath% %buildparams% -DANDROID_ABI=%abi% -DCMAKE_BUILD_TYPE=%type%
-cmake --build %buildpath% --clean-first --config %type% --target all -- -j8
-set type=release
-set buildpath=%buildprefix%/%abi%_%type%
-cmake -G "Unix Makefiles" -S . -B %buildpath% %buildparams% -DANDROID_ABI=%abi% -DCMAKE_BUILD_TYPE=%type%
-cmake --build %buildpath% --clean-first --config %type% --target all -- -j8
+call:build armeabi-v7a
 
 rem arm64-v8a
-set abi=arm64-v8a
-set type=debug
-set buildpath=%buildprefix%/%abi%_%type%
-cmake -G "Unix Makefiles" -S . -B %buildpath% %buildparams% -DANDROID_ABI=%abi% -DCMAKE_BUILD_TYPE=%type%
-cmake --build %buildpath% --clean-first --config %type% --target all -- -j8
-set type=release
-set buildpath=%buildprefix%/%abi%_%type%
-cmake -G "Unix Makefiles" -S . -B %buildpath% %buildparams% -DANDROID_ABI=%abi% -DCMAKE_BUILD_TYPE=%type%
-cmake --build %buildpath% --clean-first --config %type% --target all -- -j8
+call:build arm64-v8a
 
 rem x86
-set abi=x86
-set type=debug
-set buildpath=%buildprefix%/%abi%_%type%
-cmake -G "Unix Makefiles" -S . -B %buildpath% %buildparams% -DANDROID_ABI=%abi% -DCMAKE_BUILD_TYPE=%type%
-cmake --build %buildpath% --clean-first --config %type% --target all -- -j8
-set type=release
-set buildpath=%buildprefix%/%abi%_%type%
-cmake -G "Unix Makefiles" -S . -B %buildpath% %buildparams% -DANDROID_ABI=%abi% -DCMAKE_BUILD_TYPE=%type%
-cmake --build %buildpath% --clean-first --config %type% --target all -- -j8
+call:build x86
 
 rem x86_64
-set abi=x86_64
-set type=debug
-set buildpath=%buildprefix%/%abi%_%type%
-cmake -G "Unix Makefiles" -S . -B %buildpath% %buildparams% -DANDROID_ABI=%abi% -DCMAKE_BUILD_TYPE=%type%
-cmake --build %buildpath% --clean-first --config %type% --target all -- -j8
-set type=release
-set buildpath=%buildprefix%/%abi%_%type%
-cmake -G "Unix Makefiles" -S . -B %buildpath% %buildparams% -DANDROID_ABI=%abi% -DCMAKE_BUILD_TYPE=%type%
-cmake --build %buildpath% --clean-first --config %type% --target all -- -j8
+call:build x86_64
 
-pause
+goto EOF
+
+rem params: abi(armeabi-v7a)
+:build
+cmake -G "Unix Makefiles" -S . -B %buildprefix%/%1_release %buildparams% -DANDROID_ABI=%1 -DCMAKE_BUILD_TYPE=release
+cmake --build %buildprefix%/%1_release --clean-first --config release --target all -- -j8
+
+:EOF
--- example/build_windows.bat
+++ example/build_windows.bat
@@ -2,12 +2,16 @@ del /f /s /q "./build/windows/*.*"
 rd  /s /q "./build/windows"
 
 rem win32
-cmake -S . -B ./build/windows/win32 -G "Visual Studio 16 2019" -A win32
-cmake --build ./build/windows/win32 --clean-first --config debug --target ALL_BUILD
-cmake --build ./build/windows/win32 --clean-first --config release --target ALL_BUILD
+call:build win32
 rem x64
-cmake -S . -B ./build/windows/x64 -G "Visual Studio 16 2019" -A x64
-cmake --build ./build/windows/win32 --clean-first --config debug --target ALL_BUILD
-cmake --build ./build/windows/x64 --clean-first --config release --target ALL_BUILD
+call:build x64
 
-pause
+goto EOF
+
+rem params: platform(win32)
+:build
+cmake -S . -B ./build/windows/%1 -G "Visual Studio 16 2019" -A %1 ^
+-DCMAKE_MSVC_RUNTIME_LIBRARY="MultiThreaded$<$<CONFIG:Debug>:>"
+cmake --build ./build/windows/%1 --clean-first --config release --target ALL_BUILD
+
+:EOF
