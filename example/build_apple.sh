a49a8ff1eded7ba7a3217f94550112f0ab7b4950
gongluck <gongluck93@gmail.com>
2023-02-22 15:57:42 +0800

    update demo

--- example/build_apple.sh
+++ example/build_apple.sh
@@ -1,7 +1,18 @@
 rm -r ./build/apple/*
 
+# params: system(OSX) arch("armv7;arm64;i386;x86_64")
+function build_OSX {
+  cmake -S . -B ./build/apple/$1 -GXcode \
+    -DCMAKE_INSTALL_PREFIX=$(pwd)/bin/apple/$1 \
+    -DCMAKE_XCODE_ATTRIBUTE_ENABLE_BITCODE=NO \
+    -DCMAKE_MACOSX_BUNDLE=NO \
+    -DCMAKE_XCODE_ATTRIBUTE_PRODUCT_BUNDLE_IDENTIFIER="com.gongluck.example" \
+    -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_REQUIRED=NO
+  cmake --build ./build/apple/$1 --clean-first --target ALL_BUILD --config Release
+}
+
 # params: system(iOS) arch("armv7;arm64;i386;x86_64")
-function build {
+function build_iOS {
   cmake -S . -B ./build/apple/$1 -GXcode \
     -DCMAKE_SYSTEM_NAME=$1 \
     -DCMAKE_OSX_ARCHITECTURES=$2 \
@@ -16,4 +27,5 @@ function build {
   cmake --build ./build/apple/$1 --clean-first --target ALL_BUILD --config Release
 }
 
-build iOS "arm64;x86_64"
+build_OSX OSX
+build_iOS iOS "arm64;x86_64"
